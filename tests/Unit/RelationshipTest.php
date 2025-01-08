774ec2efa664495394d4241d6e7c04f52dd84a4c
BingWen923 <wenbing923@gmail.com>
2025-01-08 14:48:51 +1300

    add test code for phpunit

--- tests/Unit/RelationshipTest.php
+++ tests/Unit/RelationshipTest.php
@@ -4,15 +4,16 @@ namespace Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
 use App\Models\Student;
-use App\Models\Attendence;
+use App\Models\Attendance;
 
 class RelationshipTest extends TestCase
 {
     /*
       relationships in the demo
       one to one    -   student to phone
-      one to many   -   student to attendence
+      one to many   -   student to attendance
       many to many  -   student to paper
+      has one through   - student to phone to StudentIdCard
     */
     protected function setUp(): void
     {
@@ -25,6 +26,8 @@ class RelationshipTest extends TestCase
 
     public function testHasOneOfMany(): void
     {
+        echo "\n********************* test one to many relationship - has one of many *********************\n";
+        echo "    hasone() latestOfMany() oldestOfMany() \n";
         try {
             $student = Student::find(1);
             $latestAtt = $student->hasOne(Attendance::class)->latestOfMany()->first();
@@ -47,6 +50,33 @@ class RelationshipTest extends TestCase
             $this->assertEquals($maxid, $latestAtt->id, "Latest attendance ID should match the max ID.");
             $this->assertEquals($minid, $oldestAtt->id, "Oldest attendance ID should match the min ID.");
             $this->assertGreaterThanOrEqual($oldestAtt->id, $latestAtt->id, "Latest attendance ID should be greater than or equal to oldest attendance ID.");
+        } finally {
+            // Ensure cleanup is always executed
+            restore_error_handler();
+            restore_exception_handler();
+        }
+    }
+
+    public function testHasOneThrough(): void
+    {
+        echo "\n********************* test one to many relationship - HasOneThrough *********************\n";
+        echo "   hasOneThrough() \n";
+        try {
+            $r1 = Student::find(1);
+            $r2 = $r1->phone()->first(); // Fetch related Phone
+            $r3 = $r2->idcard; // Use property-like access to fetch the related StudentIdCard
+         
+            $h = Student::find(1)->phone_idcard()->first();
+
+            // Check if both $r3 and $h are not null
+            echo "\n*********************the idcard ID: " . ($r3?->id ?? 'N/A');
+            $this->assertNotNull($r3, "Student's phone's idcard should not be null, check the test data.");
+            echo "\n*********************the phone_idcard() ID: " . ($h?->id ?? 'N/A');
+            $this->assertNotNull($h, "Student's phone_idcard should not be null.");
+
+
+            // Check whether $r3->id is equal to $h->id
+            $this->assertEquals($r3->id, $h->id, "The ID of the idcard obtained via phone should match the one obtained via HasOneThrough relationship.");
         } finally {
             // Ensure cleanup is always executed
             restore_error_handler();
